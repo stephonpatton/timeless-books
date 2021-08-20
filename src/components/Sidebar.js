c424ca6919394b39ea13e84537d78a98b52825d5
Stephon Patton <spatton6@asu.edu>
2021-08-20 14:45:34 -0400

    - Changed from iconify to Material UI - Removed buttons completely for UI - Added a placeholder for logo - Changed font

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,91 +1,54 @@
 import React, { Component } from "react";
-import { Button } from "react-bootstrap";
 import { Icon } from "@iconify/react";
 import "../styling/Sidebar.css";
+
+import { Button } from "@material-ui/core";
 
 export class Sidebar extends Component {
   render() {
     return (
       <div className='sidebar-container'>
-        <div className='sidebar-library-container'>
-          <Icon
-            className='sidebar-icons'
-            icon='akar-icons:book'
-            width='30'
-            height='30'
-            hFlip={true}
-            inline={true}
-          />
-          <Button className='sidebar-buttons'>Library</Button>
+        <div className='logo-container'>
+          <img
+            alt='Timeless Books Logo'
+            src='../../assets/imgs/icons8-image-96.png'
+            id='company-logo'
+          ></img>
         </div>
 
-        <div className='sidebar-goals-container'>
-          <Icon
-            className='sidebar-icons'
-            icon='mdi:bullseye-arrow'
-            width='30'
-            height='30'
-            hFlip={true}
-            inline={true}
-          />
-          <Button className='sidebar-buttons'>Goals</Button>
-        </div>
+        <div className='sidebar-content'>
+          <div className='sidebar-library-container'>
+            <Button className='sidebar-buttons'>Library</Button>
+          </div>
 
-        <div className='sidebar-stats-container'>
-          <Icon
-            className='sidebar-icons'
-            icon='ic:baseline-query-stats'
-            width='30'
-            height='30'
-            hFlip={true}
-            inline={true}
-          />
-          <Button className='sidebar-buttons'>Statistics</Button>
-        </div>
+          <div className='sidebar-goals-container'>
+            <Button className='sidebar-buttons'>Goals</Button>
+          </div>
 
-        <div className='sidebar-shelves-container'>
-          <Icon
-            className='sidebar-icons'
-            icon='bx:bx-library'
-            width='30'
-            height='30'
-            hFlip={true}
-            inline={true}
-          />
-          <Button className='sidebar-buttons'>Shelves</Button>
-        </div>
+          <div className='sidebar-stats-container'>
+            <Button className='sidebar-buttons'>Statistics</Button>
+          </div>
 
-        <div className='sidebar-aboutus-container'>
-          <Icon
-            className='sidebar-icons'
-            icon='ant-design:team-outlined'
-            width='30'
-            height='30'
-            hFlip={true}
-            inline={true}
-          />
-          <Button className='sidebar-buttons'>About Us</Button>
-        </div>
+          <div className='sidebar-shelves-container'>
+            <Button className='sidebar-buttons'>Shelves</Button>
+          </div>
 
-        <div className='sidebar-settings-logout-container'>
-          <Icon
-            className='sidebar-icons'
-            icon='bytesize:settings'
-            width='30'
-            height='30'
-            hFlip={true}
-            inline={true}
-          />
-          <Button className='sidebar-buttons'>Settings</Button>
-          <Icon
-            className='sidebar-icons'
-            icon='ant-design:logout-outlined'
-            width='30'
-            height='30'
-            hFlip={true}
-            inline={true}
-          />
-          <Button className='sidebar-buttons'>Logout</Button>
+          <div className='sidebar-aboutus-container'>
+            <Button className='sidebar-buttons'>About Us</Button>
+          </div>
+          <div className='sidebar-logout-settings-container'>
+            <div className='sidebar-settings-container'>
+              <Button id='settings-butt' className='sidebar-buttons'>
+                Settings
+              </Button>
+            </div>
+
+            <div className='sidebar-logout-container'>
+              <Button id='logout-butt' className='sidebar-buttons'>
+                Logout
+              </Button>
+            </div>
+          </div>
         </div>
       </div>
     );
@@ -95,5 +58,6 @@ export class Sidebar extends Component {
 //TODO: Layout sidebar how it should be
 //TODO: Place sidebar how it should be
 //TODO: Add template logo/name for now
+//TODO: Add links to each sidebar item
 
 export default Sidebar;
